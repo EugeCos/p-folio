c01a563da459a43c72b5be958d1176c683e51673
unknown <madskillz@gmx.com>
2018-06-20 00:24:45 -0400

    updated projects

--- index.php
+++ index.php
@@ -88,7 +88,7 @@
             </div>
             <div class="col span-1-of-2 myBio">
                 <h3 style="color: rgb(48, 48, 48)">eugene costov</h3>
-                <p style="color: rgb(48, 48, 48)">Graduated from BrainStation Toronto web development 10-week bootcamp in December 2017. Prior to engaging in web development I spent 5 years working in interational logistics, coordinationg complex import and export projects. I opted for a career switch to webdev because I consider this path to be a fresh, fast-paced, challenging and rewarding working environment. I also love to try out the latest technologies. Good knowledge of HTML5, CSS3, React, Node.js, Express and MongoDB.</p><br>
+                <p style="color: rgb(48, 48, 48)">Graduated from BrainStation Toronto full-time web development 10-week program in December 2017. Prior to engaging in web development I spent 5 years working in interational logistics, coordinationg complex import and export projects. I opted for a career switch to webdev because I consider this path to be a fresh, fast-paced, challenging and rewarding working environment. I also love to try out the latest technologies. Excellent knowledge of React, Node.js, Express and MongoDB.</p><br>
                 <p style="color: rgb(48, 48, 48)">I would describe myself as an avid road cyclist and skier, indie-film and indie-music lover, as well as long-term pizza enthusiast. I firmly believe the best days are the snowiest ones.</p>
                 <ul class="social-icons">
                     <a href="https://ca.linkedin.com/in/eugenecostov" target="_blank" class="social-linked">
@@ -114,6 +114,22 @@
         </div>
         <div class="row js--wp-2">
             <div class="col span-1-of-3">
+                <a href="#fairmont" style="text-decoration:none">
+                    <figure class="project-box">
+                        <img src="./resources/img/projects/fairmont-cover.JPG" alt="fairmont" />
+                        <p class="text">Fairmont Property Management</p>
+                    </figure>
+                </a>
+            </div>
+            <div class="col span-1-of-3">
+                <a href="#crypto" style="text-decoration:none">
+                    <figure class="project-box">
+                        <img src="./resources/img/projects/crypto-cover.JPG" alt="crypto" />
+                        <p class="text">Crypto Currency Tracker</p>
+                    </figure>
+                </a>
+            </div>
+            <div class="col span-1-of-3">
                 <a href="#immigrate" style="text-decoration:none">
                     <figure class="project-box">
                         <img src="./resources/img/projects/imm.jpg" alt="immigrate.TO" />
@@ -121,16 +137,9 @@
                     </figure>
                 </a>
             </div>
+        </div>
 
-            <div class="col span-1-of-3">
-                <a href="#weather" style="text-decoration:none">
-                    <figure class="project-box">
-                        <img src="./resources/img/projects/weather.JPG" alt="weather" />
-                        <p class="text">Weather App</p>
-                    </figure>
-                </a>
-            </div>
-
+        <div class="row js--wp-2">
             <div class="col span-1-of-3">
                 <a href="#audio" style="text-decoration:none">
                     <figure class="project-box">
@@ -139,22 +148,11 @@
                     </figure>
                 </a>
             </div>
-
-        </div>
-        <div class="row js--wp-2">
             <div class="col span-1-of-3">
-                <a href="#snowman" style="text-decoration:none">
+                <a href="#weather" style="text-decoration:none">
                     <figure class="project-box">
-                        <img src="./resources/img/projects/snowman.jpg" alt="snowman" />
-                        <p class="text">Snowman Game</p>
-                    </figure>
-                </a>
-            </div>
-            <div class="col span-1-of-3">
-                <a href="#currency" style="text-decoration:none">
-                    <figure class="project-box">
-                        <img src="./resources/img/projects/cur.JPG" alt="currency" />
-                        <p class="text">Currency Converter</p>
+                        <img src="./resources/img/projects/weather.JPG" alt="weather" />
+                        <p class="text">Weather App</p>
                     </figure>
                 </a>
             </div>
@@ -169,6 +167,57 @@
         </div>
     </section>
 
+    <section class="section-features picture-background js--section-features" id="fairmont">
+        <div class="row">
+            <h2>Fairmont Property Management</h2>
+        </div>
+        <div class="row js--wp-1">
+            <div class="col span-1-of-2">
+                <img src="./resources/img/projects/fairmont-cover.JPG" class="appImage" alt="fairmont">
+            </div>
+            <div class="col span-1-of-2 appDescription">
+                <p><b>Fairmont Property Management</b> is a presentational platform that allows visitors to explore company services and properties. Built in React using mobile-first approach based on designer's prototype; fully responsive and compatible with all browsers. Sanity.io was used to build a custom CMS for the client, this feature allows them to easily update pictures and content.</p>
+                <br>
+                <p><b>Fairmont Property Management</b> is a single page application made with React Router.</p>
+                <div>
+                    <i class="fab fa-react appFonts"></i>&nbsp;&nbsp;&nbsp;&nbsp;
+                    <i class="fab fa-node appFonts"></i>&nbsp;&nbsp;&nbsp;&nbsp;
+                    <i class="fab fa-js-square appFonts"></i>&nbsp;&nbsp;&nbsp;&nbsp;
+                    <i class="fab fa-less appFonts"></i>&nbsp;&nbsp;&nbsp;&nbsp;
+                </div>
+                <div class="visit-project">
+                    <a href="https://fairmontmanagement.com/" target="_blank" class="btn btn-project">Visit Project</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section-features picture-background js--section-features" id="crypto">
+        <div class="row">
+            <h2>Crypto Currency Tracker</h2>
+        </div>
+        <div class="row js--wp-1">
+            <div class="col span-1-of-2">
+                <img src="./resources/img/projects/crypto-cover.JPG" class="appImage">
+            </div>
+            <div class="col span-1-of-2 appDescription">
+            <h3 class="space-between-text">&nbsp;</h3>
+                <p>A serverless web-app that allows users to look up cryptocurrencies and perform basic virtual trade manipulations (buy and sell coins).</p><br>
+                <p>Crypto currencies data from CryptoCompare API.</p>
+                <div>
+                    <i class="fab fa-js-square appFonts"></i>&nbsp;&nbsp;&nbsp;&nbsp;
+                    <i class="fab fa-react appFonts"></i>&nbsp;&nbsp;&nbsp;&nbsp;
+                    <i class="fab fa-css3-alt appFonts"></i>&nbsp;&nbsp;&nbsp;&nbsp;
+                    <i class="fab fa-less appFonts"></i>&nbsp;&nbsp;&nbsp;&nbsp;
+                    <i class="fab fa-gulp appFonts"></i>&nbsp;&nbsp;&nbsp;&nbsp;
+                </div>
+                <div class="visit-project">
+                    <a href="http://euge-cryptotracker.herokuapp.com/" target="_blank" class="btn btn-project">Visit Project</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    
     <section class="section-features picture-background js--section-features" id="immigrate">
         <div class="row">
             <h2>immigrate.TO</h2>
@@ -192,33 +241,6 @@
                     <a href="https://immigrate-to.herokuapp.com" target="_blank" class="btn btn-project">Visit Project</a>
                 </div>
             </div>
-        </div>
-    </section>
-
-    <section class="section-features picture-background js--section-features" id="weather">
-        <div class="row">
-            <h2>Weather App</h2>
-        </div>
-        <div class="row js--wp-1">
-        <div class="col span-1-of-2">
-                <img src="./resources/img/projects/weather.JPG" class="appImage">
-            </div>
-            <div class="col span-1-of-2 appDescription">
-            <h3 class="space-between-text">&nbsp;</h3>
-                <p><b>Weather APP</b> is a simple weather forecast platform. It uses Open Weather Map API to display current temperature and weather condition based on user's input. It also updates the image of the city in the box on the right, also based on user's input. Finally, it updates the background image based on the weather condition (rain, snow, haze, fog, clear, clouds etc.).</p><br>
-                <p><b>Weather APP</b> is a single page application made with React Router. It uses Axios to pull the data from API's. It uses React Lifecycle methods to avoid issues with asynchronicity.</p>
-                <div>
-                    <i class="fab fa-react appFonts"></i>&nbsp;&nbsp;&nbsp;&nbsp;
-                    <i class="fab fa-node appFonts"></i>&nbsp;&nbsp;&nbsp;&nbsp;
-                    <i class="fab fa-js-square appFonts"></i>&nbsp;&nbsp;&nbsp;&nbsp;
-                    <i class="fab fa-html5 appFonts"></i>&nbsp;&nbsp;&nbsp;&nbsp;
-                    <i class="fab fa-css3-alt appFonts"></i>&nbsp;&nbsp;&nbsp;&nbsp;
-                </div>
-                <div class="visit-project">
-                    <a href="./weather/index.html" target="_blank" class="btn btn-project">Visit Project</a>
-                </div>
-            </div>
-           
         </div>
     </section>
 
@@ -248,18 +270,18 @@
         </div>
     </section>
 
-    <section class="section-features picture-background js--section-features" id="snowman">
+    <section class="section-features picture-background js--section-features" id="weather">
         <div class="row">
-            <h2>Snowman</h2>
+            <h2>Weather App</h2>
         </div>
         <div class="row js--wp-1">
         <div class="col span-1-of-2">
-                <img src="./resources/img/projects/snowman.jpg" class="appImage">
+                <img src="./resources/img/projects/weather.JPG" class="appImage">
             </div>
             <div class="col span-1-of-2 appDescription">
             <h3 class="space-between-text">&nbsp;</h3>
-                <p><b>Snowman</b> is a less dire version of the famous paper and pencil Hangman game where player has to guess a word. With every wrong guess a part of a snowman gets melted, starting with the head. After 6 wrong guesses snowman is completely melted and game is lost. If user guesses the word, game is won.</p><br>
-                <p><b>Snowman</b> is a single page application made with React Router.</p>
+                <p><b>Weather APP</b> is a simple weather forecast platform. It uses Open Weather Map API to display current temperature and weather condition based on user's input. It also updates the image of the city in the box on the right, also based on user's input. Finally, it updates the background image based on the weather condition (rain, snow, haze, fog, clear, clouds etc.).</p><br>
+                <p><b>Weather APP</b> is a single page application made with React Router. It uses Axios to pull the data from API's. It uses React Lifecycle methods to avoid issues with asynchronicity.</p>
                 <div>
                     <i class="fab fa-react appFonts"></i>&nbsp;&nbsp;&nbsp;&nbsp;
                     <i class="fab fa-node appFonts"></i>&nbsp;&nbsp;&nbsp;&nbsp;
@@ -268,34 +290,10 @@
                     <i class="fab fa-css3-alt appFonts"></i>&nbsp;&nbsp;&nbsp;&nbsp;
                 </div>
                 <div class="visit-project">
-                    <a href="https://euge-snowman.herokuapp.com/" target="_blank" class="btn btn-project">Visit Project</a>
+                    <a href="./weather/index.html" target="_blank" class="btn btn-project">Visit Project</a>
                 </div>
             </div>
-
-        </div>
-    </section>
-
-    <section class="section-features picture-background js--section-features" id="currency">
-        <div class="row">
-            <h2>Currency Converter</h2>
-        </div>
-        <div class="row js--wp-1">
-            <div class="col span-1-of-2">
-                <img src="./resources/img/projects/cur.JPG" class="appImage">
-            </div>
-            <div class="col span-1-of-2 appDescription">
-            <h3 class="space-between-text">&nbsp;</h3>
-                <p>Rates converter with three currencies: USD, CAD and EUR. Currency data is fetched from Fixer.io API. Project created using Vanilla JS (ES6), SASS and HTML.</p><br>
-                <div>
-                    <i class="fab fa-js-square appFonts"></i>&nbsp;&nbsp;&nbsp;&nbsp;
-                    <i class="fab fa-html5 appFonts"></i>&nbsp;&nbsp;&nbsp;&nbsp;
-                    <i class="fab fa-css3-alt appFonts"></i>&nbsp;&nbsp;&nbsp;&nbsp;
-                    <i class="fab fa-sass appFonts"></i>&nbsp;&nbsp;&nbsp;&nbsp;
-                </div>
-                <div class="visit-project">
-                    <a href="./currency/index.html" class="btn btn-project">Visit Project</a>
-                </div>
-            </div>
+           
         </div>
     </section>
 
@@ -337,7 +335,7 @@
                     <a href="mailto:eugene.costov@ymail.com" class="social-email">
                         <li>
                             <i class="fas fa-envelope-square email"></i>
-                            <span>&nbsp;&nbsp;/&nbsp;eugene.costov@yahoo.com</span>
+                            <span>&nbsp;&nbsp;/&nbsp;eugene.costov@gmail.com</span>
                     </a>
                     </li>
                     <a href="https://ca.linkedin.com/in/eugenecostov" target="_blank" class="social-linked">
@@ -355,7 +353,7 @@
                 </ul>
             </div>
             <div class="col span-1-of-2">
-                <h3>Old school way</h3>
+                <h3>Send an Email</h3>
                 <div class="contact-box">
                     <div class="form-box">
                         <div class="row">
